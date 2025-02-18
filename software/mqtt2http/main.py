a5e58a80f224ba43a5176e71cbe793c745cca3cc
Astramast <azerqsdfazerqsdg@gmail.com>
2025-02-18 21:45:24 +0100

    Tue 18 Feb 21:45:24 CET 2025

--- software/mqtt2http/main.py
+++ software/mqtt2http/main.py
@@ -3,8 +3,7 @@ from HttpService import HttpService
 from MQTTSettings import MQTTSettings
 import settings
 
-mqttSettings = MQTTSettings(MQTT_SERVER, MQTT_PORT, MQTT_KEEPALIVE)
-mqttClient = MQTTClient(mqttSettings)
+mqttSettings = MQTTSettings(settings.MQTT_SERVER, settings.MQTT_PORT, MQTT_KEEPALIVE)
 httpService = HttpService(INCUBATOR_BASE_URL, INCUBATOR_SECRET)
 
 mqttClient.start()
